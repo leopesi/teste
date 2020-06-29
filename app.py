559ecfa5c7ab948485b53404724db7c7add72aee
leopesi <leopesi@yahoo.com.br>
2020-06-28 22:25:52 -0300

    redis

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 from .genius import get_popularity_songs
 import uuid
+import redis
 
 def create_app():
     app = Flask(__name__)
+    app.debug = True
     app.add_url_rule("/artist/<name>", 'artist', artist)
+    db = redis.Redis('localhost')
     return app
 
 def artist(name):
